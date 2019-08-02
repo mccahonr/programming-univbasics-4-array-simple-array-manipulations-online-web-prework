788b3ec1505cbd175ffaabb6d0090a07bbd42567
Ryan McCahon <mccahonr@yahoo.com>
2019-08-02 18:30:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,3 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 next_color = "violet"
-colors_in_the_rainbow.push(next_color)
+updated_array = colors_in_the_rainbow.push(next_color)
